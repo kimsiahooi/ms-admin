b81bae7a88edc70d452505c160b76c9d4c47be6f
kimsiahooi <kimsiahooi@gmail.com>
2025-04-11 17:27:47 +0800

    replace app name

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -1,5 +1,12 @@
 <script setup lang="ts">
 import AppLogoIcon from '@/components/AppLogoIcon.vue';
+import type { SharedData } from '@/types';
+import { usePage } from '@inertiajs/vue3';
+import { computed } from 'vue';
+
+const page = usePage<SharedData>();
+
+const appName = computed(() => page.props.name);
 </script>
 
 <template>
@@ -7,6 +14,6 @@ import AppLogoIcon from '@/components/AppLogoIcon.vue';
         <AppLogoIcon class="size-5 fill-current text-white dark:text-black" />
     </div>
     <div class="ml-1 grid flex-1 text-left text-sm">
-        <span class="mb-0.5 truncate font-semibold leading-none">Laravel Starter Kit</span>
+        <span class="mb-0.5 truncate font-semibold leading-none">{{ appName }}</span>
     </div>
 </template>
