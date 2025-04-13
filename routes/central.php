a412e4033010dd572f7aa98fbf76b7bb69adceec
kimsiahooi <kimsiahooi@gmail.com>
2025-04-13 13:39:27 +0800

    quick reformat central

--- routes/central.php
+++ routes/central.php
@@ -16,16 +16,22 @@ Route::domain($domain)->group(function () {
             return Inertia::render('Dashboard');
         })->name('dashboard');
 
-
         Route::resource('roles', RoleController::class)->except(['show']);
+        Route::prefix('roles')
+            ->name('roles.')
+            ->group(function () {
+                Route::get('audits', [UserController::class, 'audits'])->name('audits');
+            });
         Route::match(['put', 'patch'], 'roles/{role}/permissions/update', [RoleController::class, 'updatePermissions'])->name('roles.updatePermissions');
         Route::resource('users', UserController::class)->except(['show']);
-        Route::prefix('users')->name('users.')->group(function () {
-            Route::get('trashed', [UserController::class, 'trashed'])->name('trashed');
-            Route::post('{user}/restore', [UserController::class, 'restore'])->name('restore');
-            Route::delete('{user}/force-delete', [UserController::class, 'forceDelete'])->name('forceDelete');
-            Route::get('audits', [UserController::class, 'audits'])->name('audits');
-        });
+        Route::prefix('users')
+            ->name('users.')
+            ->group(function () {
+                Route::get('trashed', [UserController::class, 'trashed'])->name('trashed');
+                Route::post('{user}/restore', [UserController::class, 'restore'])->name('restore');
+                Route::delete('{user}/force-delete', [UserController::class, 'forceDelete'])->name('forceDelete');
+                Route::get('audits', [UserController::class, 'audits'])->name('audits');
+            });
         Route::resource('tenants', TenantController::class);
     });
 
