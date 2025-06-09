2fb23e967d12602fc6f1fd967befa3f11f199042
kimsiahooi <kimsiahooi@gmail.com>
2025-06-09 09:06:46 +0800

    quick fix dialog type

--- resources/js/components/shared/dialog/types/index.ts
+++ resources/js/components/shared/dialog/types/index.ts
@@ -1,7 +1,7 @@
 export type DialogMethodType<T extends string = 'store' | 'update' | 'destroy'> = T;
 
-export interface DialogType<T = null> {
-    type: DialogMethodType | null;
+export interface DialogType<T = null, U extends string = DialogMethodType> {
+    type: U | null;
     isOpen: boolean;
     title: string;
     data: T | null;
