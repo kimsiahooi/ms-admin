f8f037be3cb971a4cf743da5f8156cc5a5298b5f
Ooi Kim Siah <kimsiahooi@gmail.com>
2025-07-19 13:46:06 +0800

    quick commit

--- app/Http/Controllers/Tenant/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Tenant/Auth/RegisteredUserController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Tenant\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
+use App\Models\tenant\User;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
--- config/auth.php
+++ config/auth.php
@@ -66,7 +66,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => env('AUTH_MODEL', App\Models\Tenant\User::class),
+            'model' => env('AUTH_MODEL', App\Models\tenant\User::class),
         ],
         'admins' => [
             'driver' => 'eloquent',
